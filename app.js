cb042c1181b3dd3b12c2534632b204588e85b2c2
WeiZuJu <weizuju@gmail.com>
2026-08-21 23:55:12 +0800

    Fix fsType DRM for 字體H and bump cache buster

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
         else if (ext === 'otf') format = "format('opentype')";
         else if (ext === 'ttc') format = "format('collection'), format('truetype')";
 
-        const fileUrl = encodeURI('./src/' + font.file) + '?v=15';
+        const fileUrl = encodeURI('./src/' + font.file) + '?v=16';
         
         cssRules += `
             @font-face {
