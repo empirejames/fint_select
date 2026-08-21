b29d3d2382f2a6dcd5cf75654b3501fe33abcc5f
WeiZuJu <weizuju@gmail.com>
2026-08-21 23:52:05 +0800

    Add cache buster to font URLs to force browser reload

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
         else if (ext === 'otf') format = "format('opentype')";
         else if (ext === 'ttc') format = "format('collection'), format('truetype')";
 
-        const fileUrl = encodeURI('./src/' + font.file);
+        const fileUrl = encodeURI('./src/' + font.file) + '?v=15';
         
         cssRules += `
             @font-face {
