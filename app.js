db3b6f75285daf9fc0c9bc359f22584c7e46a4b6
WeiZuJu <weizuju@gmail.com>
2026-08-22 08:43:15 +0800

    Fix internal fsType and table errors for fonts A, C, D, G, H

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
         else if (ext === 'otf') format = "format('opentype')";
         else if (ext === 'ttc') format = "format('collection'), format('truetype')";
 
-        const fileUrl = encodeURI('./src/' + font.file) + '?v=17';
+        const fileUrl = encodeURI('./src/' + font.file) + '?v=18';
         
         cssRules += `
             @font-face {
